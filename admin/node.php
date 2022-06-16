1ad8e296e1a41b4f65b05243219b62448686bfa4
MrJuju0319 <Xxjuju0319xX@gmail.com>
2022-06-16 23:24:49 +0200

    Trad

--- admin/node.php
+++ admin/node.php
@@ -9,15 +9,15 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
-        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
+        'fqdn_not_resolvable' => 'L’adresse FQDN ou IP fournie ne résout pas une adresse IP valide.',
+        'fqdn_required_for_ssl' => 'Un nom de domaine entièrement qualifié qui résout une adresse IP publique est nécessaire pour utiliser SSL pour ce noeud.',
     ],
     'notices' => [
-        'allocations_added' => 'Allocations have successfully been added to this node.',
-        'node_deleted' => 'Node has been successfully removed from the panel.',
-        'location_required' => 'You must have at least one location configured before you can add a node to this panel.',
-        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
-        'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
-        'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
+        'allocations_added' => 'Les allocations ont bien été ajoutées à ce nœud.',
+        'node_deleted' => 'Le nœud a bien été supprimé.',
+        'location_required' => 'Vous devez avoir au moins un emplacement configuré avant de pouvoir ajouter un nœud.',
+        'node_created' => 'Création réussie d’un nouveau noeud. Vous pouvez configurer automatiquement le démon sur cette machine en visitant l’onglet \'Configuration\' tab. <strong>Avant de pouvoir ajouter des serveurs, vous devez d’abord allouer au moins une adresse IP et un port.</strong>',
+        'node_updated' => 'Les informations du nœud ont été mises à jour. Si des paramètres du démon ont été modifiés, il faudra le redémarrer pour que ces modifications prennent effet.',
+        'unallocated_deleted' => 'Suppression de tous les ports non affectés pour <code>:ip</code>.',
     ],
 ];
